df333c06fe29e5c008b79bb6d8b2348a35d8bd17
cpt <mbutkiewicz11@gmail.com>
2023-03-10 22:02:44 +0100

    Add the comments to the code for clarity

--- app/chatgpt/openai.py
+++ app/chatgpt/openai.py
@@ -1,18 +1,26 @@
+# The load_dotenv function reads the key-value pairs from a .env file and sets them as environment variables.
 from dotenv import load_dotenv
+# The os module provides a way of using operating system dependent functionality.
 import os
+# The openai module provides a Python interface to the OpenAI API.
 import openai
 
+# Load environment variables from .env file.
 load_dotenv()
 
+# Set OpenAI API key from environment variables.
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Define a function that sends a prompt to the GPT model and returns its response.
 def chatgpt_response(prompt):
+    # Call the OpenAI API to generate a completion based on the prompt.
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         temperature=1,
         max_tokens=100
     )
+    # Extract the text response from the API response dictionary.
     response_dict = response.get("choices")
     if response_dict and len(response_dict) > 0:
         prompt_response = response_dict[0].get("text")
--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -1,34 +1,48 @@
+# The load_dotenv function reads the key-value pairs from a .env file and sets them as environment variables.
 from dotenv import load_dotenv
+# The discord module is used to interact with the Discord API.
 import discord
+# The os module provides a way of using operating system dependent functionality.
 import os
+# Importing the chatgpt_response function from the app.chatgpt.openai module.
 from app.chatgpt.openai import chatgpt_response
 
+# Load environment variables from .env file.
 load_dotenv()
 
+# Get Discord bot token from the environment variables.
 discord_token = os.getenv("DISCORD_TOKEN")
 
+# Defining the MyClient class which extends the discord.Client class.
 class MyClient(discord.Client):
+
+    # The on_ready event is called when the bot has successfully logged in.
     async def on_ready(self):
         print("Logged on as {0}!".format(self.user))
 
+    # The on_message event is called whenever a message is sent in a channel that the bot can see.
     async def on_message(self, message):
         print(message.content)
+
+        # Ignore messages sent by the bot itself.
         if message.author == self.user:
             return
-        command, user_message = None, None
 
+        # Check if the message starts with a valid command.
+        command, user_message = None, None
         for text in ['/ai', '/bot', '/chatgpt']:
             if message.content.startswith(text):
                 command = message.content.split(' ')[0]
                 user_message = message.content.replace(text, '')
                 print(command, user_message)
                 break
-        
+
+        # If the message starts with a valid command, get a response from the GPT model and send it back to the channel.
         if command == '/ai' or command == '/bot' or command == '/chatgpt':
             bot_response = chatgpt_response(prompt = user_message)
             await message.channel.send(f'Answer: {bot_response}')
 
+# Create a new client instance with default intents and enable message content intents.
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = MyClient(intents=intents)
